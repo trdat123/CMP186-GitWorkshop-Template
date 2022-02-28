7cf38854cd4cad34af5a5b163dc19fa0316148ba
trdat123 <datngt1111@gmail.com>
2022-02-28 08:21:36 +0700

    add case support and calcucate expert level

---
+++ GitWorkshopProject/SalaryCalculator/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SalaryCalculator
+{
+    public class EmployeeService
+    {
+        private const decimal BASIC_SALARY = 10000000;
+
+        public static decimal CalculateSalary(EmployeeType employeeType, int years)
+        {
+            if (years < 0)
+            {
+                throw new ArgumentException("Years of experience is not valid");
+            }
+
+            decimal factor = 0;
+            
+            switch(employeeType) 
+            {
+              case employeeType.Engineer:
+                factor = 1;
+                break;
+              case employeeType.SeniorEngineer:
+                factor = 2.0;
+                break;
+              case employeeType.Manager:
+                factor = 2.0;
+                break;
+              case employeeType.SeniorManager:
+                factor = 3;
+                break;
+              case employeeType.Expert:
+                factor = 3;
+                break;
+              default:
+                break;
+            }
+
+            var salary = BASIC_SALARY * years * factor;
+
+            return salary;
+        }
+    }
+}
---
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SalaryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
+            Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
+            Console.ReadLine();
+
+            var managerSalary = EmployeeService.CalculateSalary(EmployeeType.Manager, 7);
+            Console.WriteLine(string.Format("Manager's Salary: {0}", managerSalary));
+            Console.ReadLine();
+
+            var expertSalary = EmployeeService.CalculateSalary(EmployeeType.Expert, 9);
+            Console.WriteLine(string.Format("Expert's Salary: {0}", expertSalary));
+            Console.ReadLine();
+        }
+    }
+}
